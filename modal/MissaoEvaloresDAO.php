57f0889a8865b643579d3571c256c64c2bed0f0a
geciliane <gecy.g2010@hotmail.com>
2019-11-11 21:20:05 -0300

    atualizando MissaoEValoresDAO.php

--- modal/MissaoEvaloresDAO.php
+++ modal/MissaoEvaloresDAO.php
@@ -6,8 +6,8 @@ class MissaoEValoresDAO
     public function createMissaoValores($objTO)
     {
         $conexao = new Conexao();
-        $aux = $conexao->Conexao();
-        $res = $aux->prepare("INSET INTO -tabela-(nome, razaoSocial, missao, visao, valores) VALUES (:nome, :rs, :missao, :visao, :valores)");
+        $aux = $conexao->t();
+        $res = $aux->prepare("INSERT INTO missaovalores(nome, razaoSocial, missao, visao, valores) VALUES (:nome, :rs, :missao, :visao, :valores)");
 
         $nome   = $objTO->getNome();
         $razao  = $objTO->getRazaoSocial();
@@ -15,12 +15,29 @@ class MissaoEValoresDAO
         $visao  = $objTO->getVisao();
         $vlrs   = $objTO->getValores();
 
+
         $res->bindValue(":nome", $nome);
         $res->bindValue(":rs", $razao);
         $res->bindValue(":missao", $missao);
         $res->bindValue(":visao", $visao);
         $res->bindValue(":valores", $vlrs);
-        $res->execute();
+        
+        try {
+            return $res->execute();  
+        }catch(PDOException $e) {
+            die($e->getMessage());
+        }
+
+        
+    }
+
+    public function missaovaloresReload(){
+        $conexao = new Conexao();
+        $aux = $conexao->t();
+        $query = $aux->prepare("SELECT * FROM missaovalores");
+        $query->execute();
+        $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($resultado);
     }
 }
 
