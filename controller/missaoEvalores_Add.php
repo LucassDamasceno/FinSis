bc36ddc10d9b59ee5cbfaa7fc06ea323a875e2f3
geciliane <gecy.g2010@hotmail.com>
2019-11-11 21:15:25 -0300

    atualizando missaoEvalores_Add.php

--- controller/missaoEvalores_Add.php
+++ controller/missaoEvalores_Add.php
@@ -1,6 +1,8 @@
 <?php 
-require_once('../modal/MissaoEvaloresTO');
-require_once('../modal/MissaoEvaloresDAO');
+// error_reporting(E_ALL ^ E_DEPRECATED);
+
+require_once('../modal/MissaoEvaloresTO.php');
+require_once('../modal/MissaoEvaloresDAO.php');
 
 $nome    = isset($_REQUEST['nome'])?$nome = $_REQUEST['nome'] : null;
 $razao   = isset($_REQUEST['razao'])?$razao = $_REQUEST['razao'] : null;
@@ -8,14 +10,14 @@ $missao  = isset($_REQUEST['missao'])?$missao = $_REQUEST['missao'] : null;
 $visao   = isset($_REQUEST['visao'])?$visao = $_REQUEST['visao'] : null;
 $valores = isset($_REQUEST['valores'])?$valores = $_REQUEST['valores'] : null;
 
-$objTO_Add = new DespesasTO;
+$objTO_Add = new MissaoValoresTO;
 $objTO_Add->setNome($nome);
 $objTO_Add->setRazaoSocial($razao);
 $objTO_Add->setMissao($missao);
 $objTO_Add->setVisao($visao);
 $objTO_Add->setValores($valores);
 
-$objDAO_Add = new DespesaDAO;
-$objDAO_Add-createMissaoValores($objTO_Add);
+$objDAO_Add = new MissaoEValoresDAO;
+var_dump($objDAO_Add->createMissaoValores($objTO_Add));
 
 ?>
