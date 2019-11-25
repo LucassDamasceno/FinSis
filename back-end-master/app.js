9cc9ab5a30c779526b47fc480cb465d7e322d8c0
geciliane <gecy.g2010@hotmail.com>
2019-11-25 10:59:38 -0300

    update app.js - allow-methods get, post, put, delete

--- back-end-master/app.js
+++ back-end-master/app.js
@@ -6,8 +6,10 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(function (_, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");    
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    
     next();
 });
 
@@ -21,5 +23,3 @@ app.listen(3000);
 
 
 module.exports = app;
-
-
